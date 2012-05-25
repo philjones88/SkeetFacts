eb4b39628b170a93d4e72695e1998a14f423dea7
Phil Jones <phil.jones88@gmail.com>
2012-05-25 15:42:57 +0100

    Added Fact model to store our awesome facts!

---
+++ src/SkeetFacts/Models/Fact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SkeetFacts.Models
+{
+    public class Fact
+    {
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
