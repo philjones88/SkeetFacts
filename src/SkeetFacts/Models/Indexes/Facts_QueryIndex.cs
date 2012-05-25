1281f60875820771129d7950f2ba8a09413a5276
Phil Jones <phil.jones88@gmail.com>
2012-05-25 15:56:11 +0100

    Added Facts index for querying against

---
+++ src/SkeetFacts/Models/Indexes/Facts_QueryIndex.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Raven.Client.Indexes;
+
+namespace SkeetFacts.Models.Indexes
+{
+    public class Facts_QueryIndex : AbstractIndexCreationTask<Fact>
+    {
+        public Facts_QueryIndex()
+        {
+            Map = facts => from fact in facts
+                           select new
+                                      {
+                                          fact.Id,
+                                          fact.Created
+                                      };
+        }
+    }
+}
