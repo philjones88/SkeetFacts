dfc232174c15ca27eb19228915e1747e6666bc69
Phil Jones <phil.jones88@gmail.com>
2012-05-25 15:53:36 +0100

    Added ravencontroller, profiler

---
+++ src/SkeetFacts/Controllers/RavenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+
+namespace SkeetFacts.Controllers
+{
+    public class RavenController : Controller
+    {
+        public static IDocumentStore DocumentStore { get; set; }
+
+        private IDocumentSession _ravenSession;
+
+        public IDocumentSession RavenSession
+        {
+            get { return _ravenSession ?? (_ravenSession = DocumentStore.OpenSession()); }
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            using (_ravenSession)
+            {
+                if (_ravenSession != null && filterContext.Exception == null)
+                    _ravenSession.SaveChanges();
+            }
+        }
+
+    }
+}
--- src/SkeetFacts/Global.asax.cs
+++ src/SkeetFacts/Global.asax.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+using Raven.Client.MvcIntegration;
+using SkeetFacts.Models;
 
 namespace SkeetFacts
 {
@@ -12,10 +21,7 @@ namespace SkeetFacts
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
+        public static IDocumentStore DocumentStore { get; private set; }
 
         public static void RegisterRoutes(RouteCollection routes)
         {
@@ -31,10 +37,62 @@ namespace SkeetFacts
 
         protected void Application_Start()
         {
+            // Only use the Razor view engine, this saves wasted lookups when resolving view names
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             AreaRegistration.RegisterAllAreas();
 
-            RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            InitializeDocumentStore();
+        }
+
+        private static void InitializeDocumentStore()
+        {
+            if (DocumentStore != null) return; // prevent misuse
+
+            DocumentStore = new DocumentStore
+            {
+                ConnectionStringName = "RAVENHQ_CONNECTION_STRING"
+            }.Initialize();
+
+            TryCreatingIndexesOrRedirectToErrorPage();
+
+            RavenProfiler.InitializeFor(DocumentStore, "Email", "Password", "Username");
+            //Fields to filter out of the output
+        }
+
+        private static void TryCreatingIndexesOrRedirectToErrorPage()
+        {
+            try
+            {
+                IndexCreation.CreateIndexesAsync(Assembly.GetAssembly(typeof (Fact)), DocumentStore);
+            }
+            catch (WebException e)
+            {
+                var socketException = e.InnerException as SocketException;
+                if (socketException == null)
+                    throw;
+
+                switch (socketException.SocketErrorCode)
+                {
+                    case SocketError.AddressNotAvailable:
+                    case SocketError.NetworkDown:
+                    case SocketError.NetworkUnreachable:
+                    case SocketError.ConnectionAborted:
+                    case SocketError.ConnectionReset:
+                    case SocketError.TimedOut:
+                    case SocketError.ConnectionRefused:
+                    case SocketError.HostDown:
+                    case SocketError.HostUnreachable:
+                    case SocketError.HostNotFound:
+                        HttpContext.Current.Response.Redirect("~/offline.html");
+                        break;
+                    default:
+                        throw;
+                }
+            }
         }
     }
 }
